bb7e3046802776e8ec9b1e779aadb1ebbc7a76b4
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-10-03 17:15:45 -0400

    Fix - updated heroku credentials

--- server/db/config.js
+++ server/db/config.js
@@ -1,13 +1,14 @@
 const Firebase = require('firebase')
-const { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId } = require('./credentials')
+if (process.env.NODE_ENV !== 'production') require('./credentials')
 
 const app = Firebase.initializeApp({
-  apiKey: process.env.API_KEY || apiKey,
-  authDomain: process.env.AUTH_DOMAIN || authDomain,
-  databaseURL: process.env.DATABASE_URL || databaseURL,
-  storageBucket: process.env.STORAGE_BUCKET || storageBucket,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID || messagingSenderId
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 })
+
 const db = app.database()
 
 module.exports = { db, app }
