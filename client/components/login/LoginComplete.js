99855a04ca6ddb0daeac5fe58d1f099914e5d9af
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-10-03 22:09:30 -0400

    Fix - updated login to show email instead of name

--- client/components/login/LoginComplete.js
+++ client/components/login/LoginComplete.js
@@ -3,14 +3,10 @@ import React, { Component } from 'react'
 export default class LoginComplete extends Component {
   render() {
     const { logout } = this.props
-    const { displayName, email } = this.props.user
+    const { email } = this.props.user
     return (
       <div id="user-home">
-        {
-          (displayName)
-            ? <h3>Welcome, {displayName}</h3>
-            : <h3>Welcome, {email}</h3>
-        }
+        <h3>Welcome, {email}</h3>
         <button type="button" onClick={logout}>Logout</button>
       </div>
     )
