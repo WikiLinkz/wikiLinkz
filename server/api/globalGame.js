91f5c36d25d94ae0d5dc87437218c140a0a30245
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-10-12 11:59:03 -0400

    Hardcoded title

--- server/api/globalGame.js
+++ server/api/globalGame.js
@@ -22,8 +22,8 @@ router.post('/', async (req, res, next) => {
     const gameId = await db.ref('GlobalGame').push().key
     await db.ref('GlobalGame/').set({
       gameId: gameId,
-      start: start,
-      target: target,
+      start: 'Elon_Musk',
+      target: 'Beanie_Babies',
       clickInfo: true,
       startTime: startTime,
       endTime: endTime,
