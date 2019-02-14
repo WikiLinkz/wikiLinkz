0bf03a7a4ef62915284ef692eb54ae3d48b5ce39
Andre Rivie <andre.rivie@gmail.com>
2019-02-13 23:10:04 -0500

    added max width to finish screen div

--- client/components/Finished.js
+++ client/components/Finished.js
@@ -1,22 +1,22 @@
 import React, { PureComponent } from 'react'
 
 // points utility
-const getPoints = (clicks) => {
+const getPoints = clicks => {
   let points = 1000
-  if (clicks >= 3 && clicks <= 10) points -= ((clicks - 2) * 100)
+  if (clicks >= 3 && clicks <= 10) points -= (clicks - 2) * 100
   if (clicks >= 11) points = 100
   return points
 }
 
 class Finished extends PureComponent {
   constructor() {
-    super();
+    super()
     this.state = {
       start: '',
       target: '',
       history: [],
       clicks: '',
-      won: false
+      won: false,
     }
   }
 
@@ -24,7 +24,11 @@ class Finished extends PureComponent {
     const { userStats, start, target } = this.props
     const { history, clicks, won } = userStats
     this.setState({
-      start, target, history, clicks, won
+      start,
+      target,
+      history,
+      clicks,
+      won,
     })
   }
 
@@ -32,27 +36,18 @@ class Finished extends PureComponent {
     const { start, target, history, clicks, won } = this.state
     const points = getPoints(clicks)
     return (
-      <div className='gameover-container' style={{ display: "flex", flexDirection: "column", alignItems: 'center' }}>
-        {
-          won
-            ? <h2>Way to go!</h2>
-            : <h2>Time's up</h2>
-        }
-        {
-          won
-            ? <h2>YOU REACHED THE TARGET!</h2>
-            : <h2>NICE TRY. PLAY AGAIN!</h2>
-        }
-        <div id='game-finished-container'>
+      <div
+        className="gameover-container"
+        style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      >
+        {won ? <h2>Way to go!</h2> : <h2>Time's up!</h2>}
+        {won ? <h2>You reached the target!</h2> : <h2>Nice try. Play again?</h2>}
+        <div id="game-finished-container">
           <p>Start: {start}</p>
           <p>Target: {target}</p>
           <p>History: {history.join(' => ')}</p>
           <p>Clicks: {clicks} </p>
-          {
-            won
-              ? <p>Points: {points}</p>
-              : null
-          }
+          {won ? <p>Points: {points}</p> : null}
         </div>
       </div>
     )
